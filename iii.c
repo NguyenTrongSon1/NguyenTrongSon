46fa233d893cb75b3bb7e35f2e0c45ae5f85ca6b
NguyenTrongSon <sonntth2003006@fpt.edu.vn>
2020-05-08 16:49:56 +0700

    Create iii.c

---
+++ iii.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{   int a, b, c;
+    printf ("a la:");
+    scanf ("%d", &a);
+
+    printf ("b la:");
+    scanf ("%d", &b);
+
+
+    c = (a + b)/2;
+
+    printf ("\nc la:%d", c);
+
+
+
+    return 0;
+
+
+}
