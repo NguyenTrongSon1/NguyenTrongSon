efe5f06bb504293ca2fabd9bc46b9124b01d4327
NguyenTrongSon <sonntth2003006@fpt.edu.vn>
2020-05-08 16:40:49 +0700

    Create main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main ()
+
+{
+   float $;
+   $ = 16.686;
+   printf ("ban muon doi bao nhieu $:");
+   scanf ("%f", &$);
+   printf ("ban da doi duoc %0.2f VND", $ * 16.689);
+   return 0;
+}
+
